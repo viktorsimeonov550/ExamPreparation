8df2d13a73db46d06413f427beecb77c1a9f2cf7
viktorsimeonov550 <viktor.simeonov@yahoo.com>
2023-12-20 14:32:04 +0200

    domashno

--- ConsoleApp1/OffRoadChalenge/Program.cs
+++ ConsoleApp1/OffRoadChalenge/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace OffRoadChallenge
+{
+
+
+    class Program
+    {
+        static void Main()
+        {
+            int[] initialFuel = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] index = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] neededFuel = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int count = initialFuel.Length;
+            int reachedAltitudes = 0;
+
+            for (int altitude = 1; altitude <= count; altitude++)
+            {
+                int currentFuel = initialFuel[count - altitude];
+                int consumptionIndex = index[altitude - 1];
+
+                int remainingFuel = currentFuel - consumptionIndex;
+
+                if (remainingFuel >= neededFuel[0])
+                {
+                    Console.WriteLine($"John has reached: Altitude {altitude}");
+                    reachedAltitudes = altitude;
+                }
+                else
+                {
+                    Console.WriteLine($"John did not reach: Altitude {altitude}");
+
+                    if (reachedAltitudes > 0)
+                    {
+                        Console.WriteLine("John failed to reach the top.");
+                        Console.WriteLine($"Reached altitudes: Altitude {string.Join(", ", Enumerable.Range(1, reachedAltitudes))}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("John failed to reach the top.");
+                        Console.WriteLine("John didn't reach any altitude.");
+                    }
+
+                    return;
+                }
+
+                neededFuel = neededFuel.Skip(1).ToArray();
+
+                if (altitude == count)
+                {
+                    Console.WriteLine("John has reached all the altitudes and managed to reach the top!");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/TreasureHunt/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace TreasureHunt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> treasure = Console.ReadLine().Split("|").ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Yohoho!")
+            {
+                string[] tokens = command.Split();
+                string action = tokens[0];
+
+                switch (action)
+                {
+                    case "Loot":
+                        for (int i = 1; i < tokens.Length; i++)
+                        {
+                            string item = tokens[i];
+                            if (!treasure.Contains(item))
+                            {
+                                treasure.Insert(0, item);
+                            }
+                        }
+                        break;
+
+                    case "Drop":
+                        int index = int.Parse(tokens[1]);
+                        if (index >= 0 && index < treasure.Count)
+                        {
+                            string droppedItem = treasure[index];
+                            treasure.RemoveAt(index);
+                            treasure.Add(droppedItem);
+                        }
+                        break;
+
+                    case "Steal":
+                        int count = Math.Min(int.Parse(tokens[1]), treasure.Count);
+                        List<string> stolenItems = treasure.TakeLast(count).ToList();
+                        treasure.RemoveRange(treasure.Count - count, count);
+                        Console.WriteLine(string.Join(", ", stolenItems));
+                        break;
+                }
+            }
+
+            double gain = treasure.Sum(item => item.Length) / (double)treasure.Count;
+
+            if (treasure.Count > 0)
+            {
+                Console.WriteLine($"Average treasure gain: {gain:F2} pirate credits.");
+            }
+            else
+            {
+                Console.WriteLine("Failed treasure hunt.");
+            }
+        }
+    }
+}
